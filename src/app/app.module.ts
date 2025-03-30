45767e84ef1f9489922ec1baa2137e88d5b50182
Charles J <143773361+Hyper-Text-Coffee-Pot@users.noreply.github.com>
2025-03-30 15:20:23 -0500

    Actually fixed pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,12 +33,10 @@ export function firebaseConfigFactory(configService: ConfigService)
 		IonicModule.forRoot(),
 		AppRoutingModule,
 		ReactiveFormsModule,
-  ServiceWorkerModule.register('ngsw-worker.js', {
-    enabled: !isDevMode(),
-    // Register the ServiceWorker as soon as the application is stable
-    // or after 30 seconds (whichever comes first).
-    registrationStrategy: 'registerWhenStable:30000'
-  })
+		ServiceWorkerModule.register('./ngsw-worker.js', {
+			enabled: true,
+			registrationStrategy: 'registerWhenStable:30000'
+		})
 	],
 	providers: [
 		{
