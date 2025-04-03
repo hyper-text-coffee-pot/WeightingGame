49b30bd1c82588415bb2d66893b27faee8d319ce
Charles J <143773361+Hyper-Text-Coffee-Pot@users.noreply.github.com>
2025-04-03 18:25:26 -0500

    Add versioning info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,9 @@
 // Add them to the environment variables of the hosting service.
 // Then include them in the environment.json file.
 export const environment = {
-	production: false
+	production: false,
+	version: "1.0.2",
+	versionName: "Peppy Platypus"
 };
 
 import 'zone.js/plugins/zone-error';
